6c3e55f7f594e050f3d89ecc26dd8d446a619d99
alenushka <alionxlo@gmail.com>
2022-07-02 18:27:17 +0300

    writes a script for a swiper on a tablet and mobile

--- source/js/script.js
+++ source/js/script.js
@@ -1,7 +1,11 @@
 const openBurger = document.querySelector('.page-header__burger');
 const closeButton = document.querySelector('.page-header__close');
 const navigation = document.querySelector('.page-header__nav');
-// const breakpoint = window.matchMedia('(max-width: 768px)');
+const clientsSlider = document.querySelector('.clients__slider');
+
+const breakpointD = window.matchMedia('(min-width: 1440px)');
+const breakpointT = window.matchMedia('(min-width: 768px)');
+let swiper;
 
 function showNavigation() {
   navigation.classList.add('page-header__nav--open');
@@ -22,11 +26,53 @@ function onCloseClick() {
 openBurger.addEventListener('click', onBurgerClick);
 closeButton.addEventListener('click', onCloseClick);
 
+const initSwiper = () => {
+  if (swiper) {
+    swiper.destroy(true, true);
+  }
 
-// const swiper = new Swiper('.clients__slider', {
-//   loop: true,
-//   slidesPerView: 5,
-//   autoplay: {
-//     delay: 1000,
-//   },
-// });
+  if (breakpointD.matches) {
+    swiper = new Swiper(clientsSlider, {
+      loop: true,
+      slidesPerView: 5,
+      speed: 0,
+      disableOnInteraction: false,
+      autoplay: {
+        delay: 2000,
+      },
+    });
+  } else if (breakpointT.matches) {
+    swiper = new Swiper(clientsSlider, {
+      loop: true,
+      slidesPerView: 'auto',
+      speed: 0,
+      disableOnInteraction: false,
+      autoplay: {
+        delay: 2000,
+      },
+    });
+  } else {
+    swiper = new Swiper(clientsSlider, {
+      loop: true,
+      slidesPerView: 'auto',
+      speed: 0,
+      disableOnInteraction: false,
+      autoplay: {
+        delay: 2000,
+      },
+    });
+  }
+};
+
+const initValuesSwiper = () => {
+  if (clientsSlider) {
+
+    breakpointD.addListener(initSwiper);
+    breakpointT.addListener(initSwiper);
+    initSwiper();
+  }
+};
+
+window.addEventListener('DOMContentLoaded', () => {
+  initValuesSwiper();
+})
